73332194f79eeac5b0f6add1330af37f368a9874
John Scherff <jscherff@gmail.com>
2017-09-06 11:08:52 -0700

    Consolidated composition objects into usbci directory and package

--- usbci/generic.go
+++ usbci/generic.go
@@ -12,72 +12,228 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package usbci
+package api
 
 import (
-	"reflect"
-	"strconv"
-	"strings"
+	"encoding/json"
+	"encoding/xml"
+	"errors"
 	"fmt"
 	"os"
+	"reflect"
+	"strconv"
 
 	"github.com/google/gousb"
 	"github.com/jscherff/gocmdb"
-	"github.com/jscherff/gocmdb/usbci/api"
 )
 
-// Device represents a USB device. The Device struct Desc field contains all
-// information about the device. It includes the raw device descriptor, the
-// config descriptor of the active config, and the size of the data buffer
-// required by the device for vendor commands sent via control transfer.
+const (
+	RequestDirectionOut uint8 = 0x00
+	RequestDirectionIn uint8 = 0x80
+	RequestTypeStandard uint8 = 0x00
+	RequestTypeClass uint8 = 0x20
+	RequestTypeVendor uint8 = 0x40
+	RequestRecipientDevice uint8 = 0x00
+	RequestRecipientInterface uint8 = 0x01
+	RequestRecipientEndpoint uint8 = 0x02
+	RequestRecipientOther uint8 = 0x03
+
+	RequestGetReport uint8 = 0x01
+	RequestSetReport uint8 = 0x09
+	RequestGetDescriptor uint8 = 0x06
+
+	TypeDeviceDescriptor uint16 = 0x0100
+	TypeConfigDescriptor uint16 = 0x0200
+	TypeHidDescriptor uint16 = 0x2200
+	TypeFeatureReport uint16 = 0x0300
+
+	ControlInterface uint16 = 0x0000
+
+	BufferSizeDeviceDescriptor int = 18
+	BufferSizeConfigDescriptor int = 9
+)
+
+// Generic decorates a gousb Device with Generic Properties and API.
 type Generic struct {
+
 	*gousb.Device
-	*api.Generic
+
+	HostName	string	`json:"host_name"`
+	VendorID	string	`json:"vendor_id"`
+	ProductID	string	`json:"product_id"`
+	VendorName	string	`json:"vendor_name"`
+	ProductName	string	`json:"product_name"`
+	SerialNum	string	`json:"serial_num"`
+	ProductVer	string	`json:"product_ver"`
+	SoftwareID	string	`json:"software_id"`
+
+	BusNumber	string	`json:"-" xml:"-" csv:"-" nvp:"-" compare:"-"`
+	BusAddress	string	`json:"-" xml:"-" csv:"-" nvp:"-" compare:"-"`
+	USBSpec		string	`json:"usb_spec" csv:"-" nvp:"-"`
+	USBClass	string	`json:"usb_class" csv:"-" nvp:"-"`
+	USBSubclass	string	`json:"usb_subclass" csv:"-" nvp:"-"`
+	USBProtocol	string	`json:"usb_protocol" csv:"-" nvp:"-"`
+	DeviceSpeed	string	`json:"device_speed" csv:"-" nvp:"-"`
+	DeviceVer	string	`json:"device_ver" csv:"-" nvp:"-"`
+	MaxPktSize	string	`json:"max_pkt_size" csv:"-" nvp:"-"`
+	ObjectType	string	`json:"object_type" csv:"-" nvp:"-"`
+
+	Vendor0		string	`json:"vendor0,omitempty" xml:",omitempty" csv:",omitempty" nvp:"omitempty"`
+	Vendor1		string	`json:"vendor1,omitempty" xml:",omitempty" csv:",omitempty" nvp:"omitempty"`
+	Vendor2		string	`json:"vendor2,omitempty" xml:",omitempty" csv:",omitempty" nvp:"omitempty"`
+	Vendor3		string	`json:"vendor3,omitempty" xml:",omitempty" csv:",omitempty" nvp:"omitempty"`
+	Vendor4		string	`json:"vendor4,omitempty" xml:",omitempty" csv:",omitempty" nvp:"omitempty"`
+	Vendor5		string	`json:"vendor5,omitempty" xml:",omitempty" csv:",omitempty" nvp:"omitempty"`
+	Vendor6		string	`json:"vendor6,omitempty" xml:",omitempty" csv:",omitempty" nvp:"omitempty"`
+	Vendor7		string	`json:"vendor7,omitempty" xml:",omitempty" csv:",omitempty" nvp:"omitempty"`
+	Vendor8		string	`json:"vendor8,omitempty" xml:",omitempty" csv:",omitempty" nvp:"omitempty"`
+	Vendor9		string	`json:"vendor9,omitempty" xml:",omitempty" csv:",omitempty" nvp:"omitempty"`
+
+	Changes		[][]string `json:"audit" csv:"-" nvp:"-" compare"-"`
 }
 
-// NewDevice converts an existing gousb device to a usbci Device.
-func NewDevice(gd *gousb.Device) (this *Generic, err error) {
+// NewGeneric instantiates a Generic wrapper for an existing gousb Device.
+func NewGeneric(gd *gousb.Device) (*Generic, error) {
 
-	var errs []string
+	var err error
 
-	mapi := &api.Generic{}
-	this := &Device{gd,mapi}
-
-	if err = this.Generic.Init(); err != nil {
-		errs = append(errs, "Generic")
-	}
+	this := &Generic{Device: gd}
+	errs := this.Init()
 
 	if len(errs) > 0 {
-		err = fmt.Errorf("API errors: ", strings.Join(errs, ","))
-		err = gocmdb.ErrorDecorator(err)
+
+		emsg := "initialization failures:"
+
+		for k, _ := range errs {
+			emsg = fmt.Sprintf("%s %s", emsg, k)
+		}
+
+		err = gocmdb.ErrorDecorator(errors.New(emsg))
 	}
 
 	return this, err
 }
 
-// Refresh items whose underlying values may have chanegd.
-func (this *Generic) Refresh() (err error) {
 
-	var errs []string
+// Init initializes API properties.
+func (this *Generic) Init() (errs map[string]bool) {
 
-	if err = this.Generic.Refresh(); err != nil {
-		errs = append(errs, "Generic")
+	var err error
+
+	if this.HostName, err = os.Hostname(); err != nil {
+		errs["HostName"] = true
+	}
+	if this.VendorName, err = this.Manufacturer(); err != nil {
+		errs["VendorName"] = true
+	}
+	if this.ProductName, err = this.Product(); err != nil {
+		errs["ProductName"] = true
+	}
+	if this.SerialNum, err = this.SerialNumber(); err != nil {
+		errs["SerialNum"] = true
 	}
 
-	if len(errs) > 0 {
-		err = fmt.Errorf("API errors: ", strings.Join(errs, ","))
-		err = gocmdb.ErrorDecorator(err)
+	this.VendorID = this.Desc.Vendor.String()
+	this.ProductID = this.Desc.Product.String()
+	this.BusNumber = fmt.Sprintf("%03d", this.Desc.Bus)
+	this.BusAddress = fmt.Sprintf("%03d", this.Desc.Address)
+	this.USBSpec = this.Desc.Spec.String()
+	this.USBClass = this.Desc.Class.String()
+	this.USBSubclass = this.Desc.SubClass.String()
+	this.USBProtocol = this.Desc.Protocol.String()
+	this.DeviceSpeed = this.Desc.Speed.String()
+	this.DeviceVer = this.Desc.Device.String()
+	this.MaxPktSize = strconv.Itoa(this.Desc.MaxControlPacketSize)
+	this.ObjectType = this.Type()
+
+	return errs
+}
+
+// Refresh updates properties whose underlying values may have changed.
+func (this *Generic) Refresh() (errs map[string]bool) {
+
+	var err error
+
+	if this.SerialNum, err = this.SerialNumber(); err != nil {
+		errs["SerialNum"] = true
 	}
 
-	return err
+	return errs
 }
 
 // Convenience method to retrieve device serial number.
 func (this *Generic) ID() (string) {
-	return this.Generic.ID()
+	return this.SerialNum
 }
 
 // Convenience method to help identify object type to other apps.
 func (this *Generic) Type() (string) {
-	return this.Generic.Type()
+	return reflect.TypeOf(this).String()
+}
+
+// Save saves the object to a JSON file.
+func (this *Generic) Save(fn string) (error) {
+	return gocmdb.SaveObject(*this, fn)
+}
+
+// Restore restores the object from a JSON file.
+func (this *Generic) Restore(fn string) (error) {
+	return gocmdb.RestoreObject(fn, this)
+}
+
+// Matches returns true if the objects and their properties are identical.
+func (this *Generic) Matches(i interface{}) (bool) {
+	return reflect.DeepEqual(this, i)
+}
+
+// Compare compares fields and properties and returns an array of differences.
+func (this *Generic) Compare(fn string) (ss [][]string, err error) {
+	di := new(Generic)
+	if err = di.Restore(fn); err != nil {
+		return ss, err
+	}
+	return gocmdb.CompareObjects(*this, *di)
+}
+
+// Audit calls Compare and places the results in the Changes field.
+func (this *Generic) Audit(fn string) (err error) {
+	this.Changes, err = this.Compare(fn)
+	return err
+}
+
+// SetSerialNum sets the serial number property only. Does not change the
+// underlying serial number in the descriptor or on the device.
+func (this *Generic) SetSerial(val string) {
+	this.SerialNum = val
+}
+
+// Filename constructs a convenient filename from the bus number, bus address,
+// vendor ID, and product ID. Filenames guaranteed unique on a single computer.
+func (this *Generic) Filename() (string) {
+	return fmt.Sprintf("%s-%s-%s-%s", this.BusNumber, this.BusAddress, this.VendorID, this.ProductID)
+}
+
+// Reports the hostname and serial number in CSV format.
+func (this *Generic) Bare() ([]byte) {
+	return []byte(this.HostName + "," + this.SerialNum)
+}
+
+// Reports all unfiltered fields in JSON format.
+func (this *Generic) JSON() ([]byte, error) {
+	return json.Marshal(*this)
+}
+
+// Reports all unfiltered fields in XML format.
+func (this *Generic) XML() ([]byte, error) {
+	return xml.Marshal(*this)
+}
+
+// Reports all unfiltered fields in CSV format.
+func (this *Generic) CSV() ([]byte, error) {
+	return gocmdb.ObjectToCSV(*this)
+}
+
+// Reports all unfiltered fields as name-value pairs.
+func (this *Generic) NVP() ([]byte, error) {
+	return gocmdb.ObjectToNVP(*this)
 }
--- usbci/magtek.go
+++ usbci/magtek.go
@@ -12,80 +12,382 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package usbci
+package api
 
 import (
+	"errors"
+	"fmt"
+	"math"
 	"reflect"
 	"strconv"
-	"strings"
-	"fmt"
-	"os"
+	"time"
 
 	"github.com/google/gousb"
 	"github.com/jscherff/gocmdb"
-	"github.com/jscherff/gocmdb/usbci/api"
 )
 
-// Device represents a USB device. The Device struct Desc field contains all
-// information about the device. It includes the raw device descriptor, the
-// config descriptor of the active config, and the size of the data buffer
-// required by the device for vendor commands sent via control transfer.
+const (
+	MagtekVendorID uint16 = 0x0801
+
+	SureswipeKbPID uint16 = 0x0001
+	SureswipeHidPID uint16 = 0x0002
+	MagnesafeSwipeKbPID uint16 = 0x0001
+	MagnesafeInsertKbPID uint16 = 0x0001
+	MagnesafeSwipeHidPID uint16 = 0x0011
+	MagnesafeInsertHidPID uint16 = 0x0013
+	MagnesafeWirelessHidPID uint16 = 0x0014
+
+	BufferSizeSureswipe int = 24
+	BufferSizeMagnesafe int = 60
+
+	CommandGetProp uint8 = 0x00
+	CommandSetProp uint8 = 0x01
+	CommandResetDevice uint8 = 0x02
+
+	ResultCodeSuccess uint8 = 0x00
+	ResultCodeFailure uint8 = 0x01
+	ResultCodeBadParam uint8 = 0x02
+
+	PropSoftwareID uint8 = 0x00
+	PropDeviceSN uint8 = 0x01
+	PropFactorySN uint8 = 0x03
+	PropProductVer uint8 = 0x04
+
+	DefaultSNLength int = 7
+)
+
+var (
+	BufferSizes = []int{24, 60}
+)
+
+// Magtek decorates a gousb Device with Generic and Magtek Properties and API.
 type Magtek struct {
-	*gousb.Device
-	*api.Generic
-	*api.Magtek
+
+	*Generic
+
+	BufferSize int		`json:"bufer_size"`
+	SoftwareID string	`json:"software_id"`
+	ProductVer string	`JSON:"product_ver"`
+	DeviceSN string		`json:"device_sn" csv:"-" nvp:"-"`
+	FactorySN string	`json:"factory_sn" csv:"-" nvp:"-"`
+	DescriptorSN string	`json:"descriptor_sn" csv:"-" nvp:"-"`
 }
 
-// NewDevice converts an existing gousb device to a usbci Device.
-func NewDevice(gd *gousb.Device) (this *Magtek, err error) {
+// NewMagtek instantiates a Magtek wrapper for an existing gousb Device.
+func NewMagtek(gd *gousb.Device) (*Magtek, error) {
 
-	var errs []string
+	var err error
 
-	gapi := &api.Generic{}
-	mapi := &api.Magtek{}
-	this := &Device{gd,gapi,mapi}
+	this := &Magtek{Generic: &Generic{Device: gd}}
+	errs := this.Init()
 
-	if err = this.Generic.Init(); err != nil {
-		errs = append(errs, "Generic")
-	}
-	if err = this.Magtek.Init(); err != nil {
-		errs = append(errs, "Magtek")
-	}
+	if errs["BufferSize"] {this = nil}
 
 	if len(errs) > 0 {
-		err = fmt.Errorf("API errors: ", strings.Join(errs, ","))
-		err = gocmdb.ErrorDecorator(err)
+
+		emsg := "initialization failures:"
+
+		for k, _ := range errs {
+			emsg = fmt.Sprintf("%s %s", emsg, k)
+		}
+
+		err = gocmdb.ErrorDecorator(errors.New(emsg))
 	}
 
 	return this, err
 }
 
-// Refresh items whose underlying values may have chanegd.
-func (this *Magtek) Refresh() (err error) {
+// Init initializes API properties.
+func (this *Magtek) Init() (errs map[string]bool) {
 
-	var errs []string
+	errs = this.Generic.Init()
 
-	if err = this.Generic.Refresh(); err != nil {
-		errs = append(errs, "Generic")
+	var err error
+
+	if this.BufferSize, err = this.GetBufferSize(); err != nil {
+		errs["BufferSize"] = true
+		return errs
 	}
-	if err = this.Magtek.Refresh(); err != nil {
-		errs = append(errs, "Magtek")
+	if this.SoftwareID, err = this.GetSoftwareID(); err != nil {
+		errs["SoftwareID"] = true
+	}
+	if this.ProductVer, err = this.GetProductVer(); err != nil {
+		errs["ProductVer"] = true
+	}
+	if this.DeviceSN, err = this.GetDeviceSN(); err != nil {
+		errs["DeviceSN"] = true
+	}
+	if this.FactorySN, err = this.GetFactorySN(); err != nil {
+		errs["FactorySN"] = true
+	}
+	if this.DescriptorSN, err = this.SerialNumber(); err != nil {
+		errs["DescriptorSN"] = true
 	}
 
-	if len(errs) > 0 {
-		err = fmt.Errorf("API errors: ", strings.Join(errs, ","))
-		err = gocmdb.ErrorDecorator(err)
+	this.SerialNum = this.DeviceSN
+	this.ObjectType = this.Type()
+
+	this.Vendor0 = "BufferSize:" + strconv.Itoa(this.BufferSize)
+	this.Vendor1 = "DeviceSN:" + this.DeviceSN
+	this.Vendor2 = "FactorySN:" + this.FactorySN
+	this.Vendor3 = "DescriptorSN:" + this.DescriptorSN
+
+	return errs
+}
+
+// Refresh updates API properties whose values may have changed.
+func (this *Magtek) Refresh() (errs map[string]bool) {
+
+	errs = this.Generic.Refresh()
+
+	var err error
+
+	if this.DeviceSN, err = this.GetDeviceSN(); err != nil {
+		errs["DeviceSN"] = true
+	}
+	if this.FactorySN, err = this.GetFactorySN(); err != nil {
+		errs["FactorySN"] = true
+	}
+	if this.DescriptorSN, err = this.SerialNumber(); err != nil {
+		errs["DescriptorSN"] = true
 	}
 
-	return err
+	this.SerialNum = this.DeviceSN
+
+	return errs
 }
 
 // Convenience method to retrieve device serial number.
 func (this *Magtek) ID() (string) {
-	return this.Magtek.ID()
+	return this.SerialNum
 }
 
 // Convenience method to help identify object type to other apps.
 func (this *Magtek) Type() (string) {
-	return this.Magtek.Type()
+	return reflect.TypeOf(this).String()
+}
+
+// DeviceSN retrieves the device configurable serial number from NVRAM.
+func (this *Magtek) GetDeviceSN() (string, error) {
+	return this.getProperty(PropDeviceSN)
+}
+
+// FactorySN retrieves the device factory serial number from NVRAM.
+func (this *Magtek) GetFactorySN() (string, error) {
+	val, err := this.getProperty(PropFactorySN)
+	if len(val) <= 1 {val = ""}
+	return val, err
+}
+
+// SoftwareID retrieves the software ID of the device from NVRAM.
+func (this *Magtek) GetSoftwareID() (string, error) {
+	return this.getProperty(PropSoftwareID)
+}
+
+// ProductVer retrieves the product version of the device from NVRAM.
+func (this *Magtek) GetProductVer() (string, error) {
+	val, err := this.getProperty(PropProductVer)
+	if len(val) <= 1 {val = ""}
+	return val, err
+}
+
+// SetDeviceSN sets the device configurable serial number in NVRAM.
+func (this *Magtek) SetDeviceSN(val string) (error) {
+	return this.setProperty(PropDeviceSN, val)
+}
+
+// EraseDeviceSN removes the device configurable serial number from NVRAM.
+func (this *Magtek) EraseDeviceSN() (error) {
+	return this.setProperty(PropDeviceSN, "")
+}
+
+// SetFactorySN sets the device factory device serial number in NVRAM.
+// This will fail with result code 07 if serial number is already set.
+func (this *Magtek) SetFactorySN(val string) (error) {
+	return this.setProperty(PropFactorySN, val)
+}
+
+// CopyFactorySN copies 'length' characters from the device factory
+// serial number to the configurable serial number in NVRAM.
+func (this *Magtek) CopyFactorySN(n int) (error) {
+
+	val, err := this.GetFactorySN()
+
+	if err != nil {
+		return gocmdb.ErrorDecorator(err)
+	}
+
+	if len(val) == 0 {
+		return gocmdb.ErrorDecorator(fmt.Errorf("no factory serial number"))
+	}
+
+	n = int(math.Min(float64(n), float64(len(val))))
+	err = this.SetDeviceSN(val[:n])
+
+	return err
+}
+
+// Reset overides inherited Reset method with a low-level vendor reset.
+func (this *Magtek) Reset() (error) {
+
+	data := make([]byte, this.BufferSize)
+	data[0] = CommandResetDevice
+
+	_, err := this.Control(
+		RequestDirectionOut + RequestTypeClass + RequestRecipientDevice,
+		RequestSetReport,
+		TypeFeatureReport,
+		ControlInterface,
+		data)
+
+	if err != nil {
+		err = gocmdb.ErrorDecorator(err)
+	}
+
+	data = make([]byte, this.BufferSize)
+
+	_, err = this.Control(
+		RequestDirectionIn + RequestTypeClass + RequestRecipientDevice,
+		RequestGetReport,
+		TypeFeatureReport,
+		ControlInterface,
+		data)
+
+	if err != nil {
+		return gocmdb.ErrorDecorator(err)
+	}
+
+	if data[0] > 0x00 {
+		err = gocmdb.ErrorDecorator(fmt.Errorf("command error: %d", int(data[0])))
+	}
+
+	time.Sleep(5 * time.Second)
+
+	return err
+}
+
+// GetBufferSize uses trial and error to find the control transfer data
+// buffer size of the device. Failure to use the correct size for control
+// transfers carrying vendor commands will result in a LIBUSB_ERROR_PIPE
+// error.
+func (this *Magtek) GetBufferSize() (size int, err error) {
+
+	var rc int
+
+	for _, size = range BufferSizes {
+
+		data := make([]byte, size)
+		copy(data, []byte{CommandGetProp, 0x01, PropSoftwareID})
+
+		rc, err = this.Control(
+			RequestDirectionOut + RequestTypeClass + RequestRecipientDevice,
+			RequestSetReport,
+			TypeFeatureReport,
+			ControlInterface,
+			data)
+
+		if err != nil {continue}
+
+		rc, err = this.Control(
+			RequestDirectionIn + RequestTypeClass + RequestRecipientDevice,
+			RequestGetReport,
+			TypeFeatureReport,
+			ControlInterface,
+			data)
+
+		if err != nil {continue}
+
+		if rc == size {
+			break
+		}
+	}
+
+	if err != nil {
+		err = gocmdb.ErrorDecorator(fmt.Errorf("unsupported device"))
+	}
+
+	return size, err
+}
+
+// getProperty retrieves a property from device NVRAM using low-level commands.
+func (this *Magtek) getProperty(id uint8) (val string, err error) {
+
+	data := make([]byte, this.BufferSize)
+	copy(data, []byte{CommandGetProp, 0x01, id})
+
+	_, err = this.Control(
+		RequestDirectionOut + RequestTypeClass + RequestRecipientDevice,
+		RequestSetReport,
+		TypeFeatureReport,
+		ControlInterface,
+		data)
+
+	if err != nil {
+		return val, gocmdb.ErrorDecorator(err)
+	}
+
+	data = make([]byte, this.BufferSize)
+
+	_, err = this.Control(
+		RequestDirectionIn + RequestTypeClass + RequestRecipientDevice,
+		RequestGetReport,
+		TypeFeatureReport,
+		ControlInterface,
+		data)
+
+	if err != nil {
+		return val, gocmdb.ErrorDecorator(err)
+	}
+
+	if data[0] > 0x00 {
+		return val, gocmdb.ErrorDecorator(fmt.Errorf("command error: %d", int(data[0])))
+	}
+
+	if data[1] > 0x00 {
+		val= string(data[2:int(data[1])+2])
+	}
+
+	return val, err
+}
+
+// setProperty configures a property in device NVRAM using low-level commands.
+func (this *Magtek) setProperty(id uint8, val string) (err error) {
+
+	data := make([]byte, this.BufferSize)
+	copy(data[0:], []byte{CommandSetProp, uint8(len(val)+1), id})
+	copy(data[3:], val)
+
+	_, err = this.Control(
+		RequestDirectionOut + RequestTypeClass + RequestRecipientDevice,
+		RequestSetReport,
+		TypeFeatureReport,
+		ControlInterface,
+		data)
+
+	if err != nil {
+		return gocmdb.ErrorDecorator(err)
+	}
+
+	data = make([]byte, this.BufferSize)
+
+	_, err = this.Control(
+		RequestDirectionIn + RequestTypeClass + RequestRecipientDevice,
+		RequestGetReport,
+		TypeFeatureReport,
+		ControlInterface,
+		data)
+
+	if err != nil {
+		return gocmdb.ErrorDecorator(err)
+	}
+
+	if data[0] > 0x00 {
+		err = gocmdb.ErrorDecorator(fmt.Errorf("command error: %d", int(data[0])))
+	}
+
+	if err == nil {
+		this.Refresh()
+	}
+
+	return err
 }

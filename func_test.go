ccbf5a8430fabdaa159bc2d5753f6fd893181d28
John Scherff <jscherff@gmail.com>
2017-10-07 21:56:28 -0700

    Modified tests to accommodate new FirmwareVer field

--- func_test.go
+++ func_test.go
@@ -1,17 +1,3 @@
-// Copyright 2017 John Scherff
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package gocmdb
 
 import (
@@ -42,10 +28,9 @@ func TestGetterMethods(t *testing.T) {
 
 func TestFilenameMethod(t *testing.T) {
 
-	fileName := fmt.Sprintf(`%03d-%03d-%03d-%s-%s`,
-                mag1.BusNumber,
-                mag1.BusAddress,
+	fileName := fmt.Sprintf(`%03d-%03d-%s-%s`,
                 mag1.PortNumber,
+                mag1.BusNumber,
                 mag1.VendorID,
                 mag1.ProductID,
         )
